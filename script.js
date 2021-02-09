e16efc717739d85d595e05b2e6438a66de5da6cd
Firsta Adi Pradana <firstaadi@hotmail.com>
2021-02-09 15:24:38 +0700

    Remove Alert

--- script.js
+++ script.js
@@ -21,7 +21,6 @@ function myFunction() {
     copyText.select();
     copyText.setSelectionRange(0, 99999);
     document.execCommand("copy");
-    alert("Copied the text: " + copyText.value);
     }
 
 
