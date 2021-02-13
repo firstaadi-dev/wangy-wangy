7d9acd2c0e7873f61359189b845385ce68fefa37
Firsta Adi Pradana <firstaadi@hotmail.com>
2021-02-13 15:04:05 +0700

    styling update

---
+++ vendor/composer/installed.php
@@ -0,0 +1,24 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => 'dev-master',
+    'version' => 'dev-master',
+    'aliases' => 
+    array (
+    ),
+    'reference' => 'aaebeea355ea38b7d51fa4f0d0e6bd9ac526990f',
+    'name' => '__root__',
+  ),
+  'versions' => 
+  array (
+    '__root__' => 
+    array (
+      'pretty_version' => 'dev-master',
+      'version' => 'dev-master',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'aaebeea355ea38b7d51fa4f0d0e6bd9ac526990f',
+    ),
+  ),
+);
